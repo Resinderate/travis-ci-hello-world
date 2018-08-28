bacd84814ca86ce3056caff84047f41b5f760e80
Ronan Murphy <ronan.murphy@originmarkets.com>
2018-08-28 23:42:28 +0100

    Added src and tests.

---
+++ src/unique.py
@@ -0,0 +1,2 @@
+def is_unique(sample: str) -> bool:
+    return len(sample) == len(set(sample))
---
+++ test/test_unique.py
@@ -0,0 +1,15 @@
+import unittest
+
+from src.unique import is_unique
+
+
+class TestUnique(unittest.TestCase):
+
+    def test_sparse(self):
+        assert is_unique("dermatoglyphics")
+
+    def test_dupes(self):
+        assert not is_unique("ronan")
+
+    def test_case_sensitive(self):
+        assert is_unique("Unique")
